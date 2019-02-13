310f6fec964ee6571dfb1327047338622bfd61ef
keprom <kokshetau.energo.prom@mail.ru>
2019-02-13 10:18:04 +0600

    added migration

--- system/application/controllers/billing.php
+++ system/application/controllers/billing.php
@@ -3976,7 +3976,7 @@ where firm_id={$this->uri->segment(3)} and data_finish is null";
     public function migration()
     {
         $data['report'] = $this->db->get("shell.migration")->result();
-        $this->export_to_excel("other_reports/migration", $data,"ленинградский_пэу");
+        $this->export_to_excel("other_reports/migration", $data,"келлеровский_пэу");
 //        $this->load->view("other_reports/migration", $data);
     }
 
